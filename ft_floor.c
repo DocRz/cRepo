701cb41c1d6eec895984255aeb5274665b396e8e
Andrew RZEPECKI <anrzepec@e1r11p1.42.fr>
2019-02-08 13:13:56 +0100

    ft_printf and sub functions added to libft

--- ft_floor.c
+++ ft_floor.c
@@ -1,18 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_fmod.c                                          :+:      :+:    :+:   */
+/*   ft_floor.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anrzepec <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/02/04 10:25:08 by anrzepec          #+#    #+#             */
-/*   Updated: 2019/02/08 13:11:37 by anrzepec         ###   ########.fr       */
+/*   Created: 2019/02/08 13:10:00 by anrzepec          #+#    #+#             */
+/*   Updated: 2019/02/08 13:11:22 by anrzepec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-long double			ft_fmod(long double nb, long double mod)
+long long 		ft_floor(long double nb)
 {
-	return (nb - mod * ft_floor(nb / mod));
+	long long	nbi;
+
+	nbi = (long long)nb;
+	return (nb < nbi ? nbi - 1 : nbi);
 }
